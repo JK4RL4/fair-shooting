56dc9cdd1ff1435addabd2771b87b948f053766b
JK <51192505+JKSV84@users.noreply.github.com>
2021-03-12 09:47:06 +0100

    v0.1

---
+++ api/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+router.use(express.urlencoded({extended: false}));
+router.use(express.json());
+
+router.get("/apiData/:collection", function(req, res, next) {
+  let dbConnection = req.app.locals.db;
+  let collection = req.params.collection;
+
+  dbConnection.collection(collection).find().toArray(function(err, data) {
+    if(err !== null) {
+      console.log(err),
+      res.send({mensaje: "Ha habido un error. " + err } );
+    } else {
+      res.send(data);
+    } 
+  })
+});
+
+router.put("/apiData/Best5", function(req, res, next) {
+  let dbConnection = req.app.locals.db;
+  let newOther5 = req.body.records;
+
+  dbConnection.collection("Best5").updateOne({"playerRecords.name": "other5"}, {$set: {"playerRecords.array": newOther5}}, function(err, data) {
+    if(err !== null) {
+  console.log(err),
+  res.send({mensaje: "Ha habido un error. " + err } );
+  } else {
+    res.send(data);
+  } 
+  })
+});
+
+module.exports = router;
